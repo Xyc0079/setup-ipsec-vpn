92489f842af3676708edd0d6c47b24a9ce19db03
Blaze.Xu <xyc0079@vip.sina.com>
2019-12-27 18:36:40 +0800

    Update vpnsetup.sh

--- vpnsetup.sh
+++ vpnsetup.sh
@@ -24,9 +24,9 @@
 # - All values MUST be placed inside 'single quotes'
 # - DO NOT use these special characters within values: \ " '
 
-YOUR_IPSEC_PSK=''
-YOUR_USERNAME=''
-YOUR_PASSWORD=''
+YOUR_IPSEC_PSK='OnlyForCS20191226'
+YOUR_USERNAME='cs57853'
+YOUR_PASSWORD='87654321'
 
 # Important notes:   https://git.io/vpnnotes
 # Setup VPN clients: https://git.io/vpnclients
